0b08a3e1b80b3a5ab772f65766b7492b5314549a
Mathis Olieu <mathis.olieu@etu.univ-grenoble-alpes.fr>
2018-04-09 07:43:14 +0000

    modif prof

--- app/components/Machine.js
+++ app/components/Machine.js
@@ -6,13 +6,25 @@
 import React from 'react';
 
 class Machine extends React.Component {
+  handleClick() {
+    console.log("blbl");
+    console.log(this.props.name);
+     this.props.handleStatusChange(this.props.index);
+  }
+
+  
   render() {
     // Dans tous les cas, afficher
     return (
         <div
         className= { this.props.isActive ? "machine active" : "machine" }>
           {this.props.name}
+          <button onClick={(e) => this.handleClick(e)} type="button" className="btn">
+          Toggle
+          </button>
+      
         </div>
+        
       )
   }
 }
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,14 +12,78 @@ import Footer from '../../components/Footer.js';
 import '../../css/styles.css';
 // On peut aussi importer du CSS de la meme facon.
 
-export default function App() {
-  return (
-    <div>
-      <Header/>
-      <Machine name="Machine 1" isActive/>
-      <Machine name="Machine 2"/>
-      <Machine name="Machine 3" isActive/>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+
+    this.state = {
+      machines: [
+        {
+          id: 0,
+          name: "Machine à café",
+          isActive: true
+        },
+        {
+          id: 1,
+          name: "Machine à thé",
+          isActive: false
+        },
+        {
+          id: 2,
+          name: "Machine à frappucino",
+          isActive: true
+        },
+        {
+          id: 3,
+          name: "Machine à citron",
+          isActive: true
+        }
+      ]
+    };
+    
+    
+  }
+  
+    // Méthode pour activer une machine
+  handleStatusChange(key) {
+    // 1. On copie le state existant
+    const machines = { ...this.state.machines };
+    // 2. On modifie le status de CETTE machine
+    if (machines[key].isActive == true)
+    {machines[key].isActive = false;}
+    else
+    {machines[key].isActive = true;}
+    // Pour vérifier la nouvelle collection dans la console :
+    console.log({ machines });
+
+    // 3. On applique cette nouvelle collection au state
+    this.setState({ machines });
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <Header/>
+          {/*Conteneur de notre liste*/}
+          <div className="machines-list">
+            {/*Boucle sur notre collection de machines*/}
+            {Object
+                .keys(this.state.machines)
+                .map(key =>
+                // Le composant Machine s'affichera autant de fois
+                // qu'il y a d'objets dans la collection.
+                <Machine name={this.state.machines[key].name}
+                         key={this.state.machines[key].id}
+                         index={this.state.machines[key].id}
+                         handleStatusChange={this.handleStatusChange}
+                         isActive={this.state.machines[key].isActive}/>
+              )}
+          </div>
+        <Footer/>
+      </div>
+    );
+  }
 }
+export default App;
